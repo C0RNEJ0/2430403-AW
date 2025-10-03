9106be187c2a4dbe681aa2394143b3f9504e951a
Jose Guadalupe Cornejo Alva <2430403@upv.edu.mx>
2025-10-03 07:36:28 -0600

    push

--- practica no.5/app.js
+++ practica no.5/app.js
@@ -9,7 +9,7 @@ let tareas = JSON.parse(localStorage.getItem("tasks")) || []; // Lista de tareas
 
 
 
-//  Referencias a Elementos del DOM ---
+//  Referencias a Elementos del DOM ---s
 
 const formatologin = document.getElementById("formatologin"); // Formulario de Login
 const errorMessageLogin = document.getElementById("error-message"); // Mensaje de error en Login
@@ -69,25 +69,25 @@ if (formatologin) { // Verifica que el formulario de login exista
     formatologin.addEventListener("submit", function(event) { // Maneja el evento de envío del formulario
         event.preventDefault(); // Previene el envío por defecto del formulario
 
-        const usuarioEmail = document.getElementById("username").value.trim(); // Obtiene y limpia el valor del correo
+        const usuariogmail = document.getElementById("username").value.trim(); // Obtiene y limpia el valor del correo
         const contrasena = document.getElementById("password").value; // Obtiene el valor de la contraseña
  
-        if (!validarCorreo(usuarioEmail))  { // Valida el formato del correo
+        if (!validarCorreo(usuariogmail))  { // Valida el formato del correo
              errorMessageLogin.textContent = "favor de ingresar un correo valido"; // Mensaje de error si no es un correo valido
             return;
         }
 
         const usuarioEncontrado = usuariosRegistrados.find(user =>  // Busca el usuario en la lista
-            user.correo === usuarioEmail && user.contrasena === contrasena // Verifica correo y contraseña
+            user.correo === usuariogmail && user.contrasena === contrasena // Verifica correo y contraseña
         );
 
         if (usuarioEncontrado) { 
             // Inicia sesión y muestra el modal
-            localStorage.setItem("usuarioLogueado", usuarioEmail); // Guarda el usuario logueado en localStorage
+            localStorage.setItem("usuarioLogueado", usuariogmail); // Guarda el usuario logueado en localStorage
             
             errorMessageLogin.textContent = "Inicio de sesión exitoso"; // Mensaje de éxito
             
-            cuerpoMensajeModal.textContent = `Bienvenido, ${usuarioEmail}!`; // Mensaje de bienvenido al usuario
+            cuerpoMensajeModal.textContent = `Bienvenido, ${usuariogmail}!`; // Mensaje de bienvenido al usuario
             modalBienvenida.show(); 
 
         } else { // Si no se encuentra el usuario
