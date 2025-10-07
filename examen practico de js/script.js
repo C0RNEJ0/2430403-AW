26c6be911ebb7bbf2fdf1636d9a1ceff5de533a4
Jose Guadalupe Cornejo Alva <2430403@upv.edu.mx>
2025-10-07 08:00:38 -0600

    .

--- examen practico de js/script.js
+++ examen practico de js/script.js
@@ -2,20 +2,18 @@ function registrar() {
   let nombre = document.getElementById("nombre").value;
   let correo = document.getElementById("correo").value;
   let contrasena = document.getElementById("contrasena").value;
-  let confirmar = document.getElementById("confirmar-contrasena").value;
+  let confirmar = document.getElementById("confirmar").value;
   let mensaje = document.getElementById("mensaje");
-  let validarCorreo = document.getElementById("validarCorreo")
-  //let =  numero_totaldecaracteres = "correo@1gmail.com";
 
-
-  
-
-  if (nombre === "" || correo === "" || contrasena === ""|| confirmar ==="") {
-    
-    
-    
-    mensaje.textContent = "Por favor completa los campos verifica si la contraseña tiene mas 6 caracteres.";
+  if (!nombre || !correo || !contrasena || !confirmar) {
+    mensaje.textContent = "Completa los campos.";
+  } else if (!correo.includes("@") || !correo.includes(".")) {
+    mensaje.textContent = "Correo no válido.";
+  } else if (contrasena.length < 6) {
+    mensaje.textContent = "La contraseña debe tener 6 de algo.";
+  } else if (contrasena !== confirmar) {
+    mensaje.textContent = "verfica las contra.";
   } else {
-    mensaje.textContent = "ingresaste con exito, " + nombre + "!";
+    mensaje.textContent = "Bienvenido, " + nombre + "!";
   }
 }
