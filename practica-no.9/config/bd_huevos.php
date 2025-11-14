4c8af99e7e6e41e85d9b4afbfe17f877ac7cee53
Jose Guadalupe Cornejo Alva <2430403@upv.edu.mx>
2025-11-13 19:12:43 -0600

    aresdewfwdsfds!

--- practica-no.9/config/bd_huevos.php
+++ practica-no.9/config/bd_huevos.php
@@ -1,13 +1,12 @@
 <?php
 
-// Configuración de la base de datos (en español)
+// Configuración de la base de datos
 
 define('BD_HOST', '127.0.0.1');
 define('BD_USUARIO', 'admin');
 define('BD_CONTRASENA', 'e8d0055b61beef5a1681ee280703da98497636b40340afca');
 define('BD_NOMBRE', 'clinica_cornejo');
 
-// Crear conexión (función en español)
 function obtenerConexion() {
     $conexion = new mysqli(BD_HOST, BD_USUARIO, BD_CONTRASENA, BD_NOMBRE);
 
@@ -19,7 +18,6 @@ function obtenerConexion() {
     return $conexion;
 }
 
-// Mantener compatibilidad con código existente
 function getConnection() {
     return obtenerConexion();
 }
